34536c8fbcf3b479ff962e043ef0218e9104207d
Nishita Solanki <nishitaslnk@gmail.com>
2025-03-31 23:31:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const audio = document.getElementById("audio");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const progress = document.getElementById("progress");
+const volume = document.getElementById("volume");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const cover = document.getElementById("cover");
+
+// Song List
+const songs = [
+    { title: "Can't Help Falling in Love", artist: " Elvis Presley", src: "Songs/Elvis Presley - Can't Help Falling In Love (Official Audio).mp3", cover: "https://upload.wikimedia.org/wikipedia/en/3/3c/Can%27t_Help_Falling_in_Love_by_Elvis_Presley_US_picture_sleeve.png" },
+    { title: "this is what falling in love feels like", artist: "JVKE",src: "Songs/JVKE - this is what falling in love feels like (Lyrics).mp3", cover: "https://i.scdn.co/image/ab67616d0000b273fc4add7266fbfa07c6b16b1c" }
+];
+
+let songIndex = 0;
+
+// Load Song
+function loadSong(song) {
+    title.innerText = song.title;
+    artist.innerText = song.artist;
+    audio.src = song.src;
+    cover.src = song.cover;
+}
+
+// Play & Pause
+function togglePlay() {
+    if (audio.paused) {
+        audio.play();
+        playBtn.innerHTML = '<img src="https://png.pngtree.com/png-clipart/20190903/original/pngtree-pause-icon-png-image_4419908.jpg">';
+    } else {
+        audio.pause();
+        playBtn.innerHTML = '<img src="https://icons.veryicon.com/png/o/internet--web/web-video-clip/play-332.png">';
+    }
+}
+
+// Next & Previous
+function nextSong() {
+    songIndex = (songIndex + 1) % songs.length;
+    loadSong(songs[songIndex]);
+    audio.play();
+}
+
+function prevSong() {
+    songIndex = (songIndex - 1 + songs.length) % songs.length;
+    loadSong(songs[songIndex]);
+    audio.play();
+}
+
+// Progress Bar
+audio.addEventListener("timeupdate", () => {
+    progress.value = (audio.currentTime / audio.duration) * 100;
+});
+
+progress.addEventListener("input", () => {
+    audio.currentTime = (progress.value / 100) * audio.duration;
+});
+
+// Volume Control
+volume.addEventListener("input", () => {
+    audio.volume = volume.value;
+});
+
+// Event Listeners
+playBtn.addEventListener("click", togglePlay);
+nextBtn.addEventListener("click", nextSong);
+prevBtn.addEventListener("click", prevSong);
+
+// Load First Song
+loadSong(songs[songIndex]);
